5aefa6b8f74739e6a6e9841cb84bbc980c193d51
Guillaume Almyre <zazarlaliche@gmail.com>
2017-05-06 02:01:35 +0200

    First unreal commit

---
+++ Source/EscapeProject/EscapeProjectGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "EscapeProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ESCAPEPROJECT_API AEscapeProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
